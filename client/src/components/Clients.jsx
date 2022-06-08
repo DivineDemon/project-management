613e9b560f5ceae8e6efa58227c01fd362ab578b
DivineDemon <supame123@gmail.com>
2022-06-08 22:22:42 +0400

    Displaying Clients! ✨

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,7 @@
 import { gql, useQuery } from "@apollo/client";
 
+import ClientRow from "./ClientRow";
+
 const GET_CLIENTS = gql`
   query getClients {
     clients {
@@ -16,7 +18,27 @@ function Clients() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Something went wrong!</p>;
 
-  return <>{!loading && !error && <h1>Clients</h1>}</>;
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Options</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <ClientRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
 
 export default Clients;
