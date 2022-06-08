3faa668e3e8ca695509e56b95d6cda6e63551161
DivineDemon <supame123@gmail.com>
2022-06-09 01:30:22 +0400

    Add Project! ✨

---
+++ client/src/mutations/projectMutations.js
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+
+export const ADD_PROJECT = gql`
+  mutation AddProject(
+    $name: String!
+    $desc: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(name: $name, desc: $desc, status: $status, clientId: $clientId) {
+      id
+      name
+      desc
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
