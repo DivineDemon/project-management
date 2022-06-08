4407698fddf6ddde5e5e03b5a27e0820331ec7cc
DivineDemon <supame123@gmail.com>
2022-06-09 00:53:59 +0400

    Project Expanded View! ✨

--- client/src/queries/projectQueries.js
+++ client/src/queries/projectQueries.js
@@ -9,3 +9,20 @@ export const GET_PROJECTS = gql`
     }
   }
 `;
+
+export const GET_PROJECT = gql`
+  query getProject($id: ID!) {
+    project(id: $id) {
+      id
+      name
+      desc
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
