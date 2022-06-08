f13f3f6bc3eea1e5538fe5d2cd4753f23559ad96
DivineDemon <supame123@gmail.com>
2022-06-08 22:37:47 +0400

    Delete Client! ✨

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,8 +1,8 @@
 import { useQuery } from "@apollo/client";
 
 import ClientRow from "./ClientRow";
-import { GET_CLIENTS } from "./../queries/clientQueries";
 import Spinner from "./Spinner";
+import { GET_CLIENTS } from "./../queries/clientQueries";
 
 function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS);
